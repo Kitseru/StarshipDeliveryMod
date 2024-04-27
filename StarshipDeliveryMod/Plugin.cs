985a17529aab87224d784ceaf2bf25cc0e78a6db
Laventin <33801288+Kitseru@users.noreply.github.com>
2024-04-28 00:28:17 +0200

    Udpate version number

--- StarshipDeliveryMod/Plugin.cs
+++ StarshipDeliveryMod/Plugin.cs
@@ -15,7 +15,7 @@ namespace StarshipDeliveryMod
     {
         private const string modGUID = "Laventin.StarshipDeliveryMod";
         private const string modName = "StarshipDelivery";
-        private const string modVersion = "1.0.0";
+        private const string modVersion = "1.0.1";
 
         private readonly Harmony harmony = new(modGUID);
 
