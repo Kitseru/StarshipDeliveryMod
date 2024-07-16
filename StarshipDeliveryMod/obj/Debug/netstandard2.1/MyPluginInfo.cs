6070d64f8efab0b814f63732f943ad5ac3326421
Laventin <33801288+Kitseru@users.noreply.github.com>
2024-07-17 00:52:03 +0200

    Delete StarshipDeliveryMod/obj directory

--- StarshipDeliveryMod/obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace StarshipDeliveryMod
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "Laventin.StarshipDeliveryMod";
-        public const string PLUGIN_NAME = "StarshipDelivery";
-        public const string PLUGIN_VERSION = "1.1.1";
-    }
-}
